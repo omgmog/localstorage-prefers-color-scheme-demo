8db1d2797b7d9632c16bdd8eccd5b5e6ea5708ce
Max Glenister <moglenstar@gmail.com>
2022-06-15 17:52:39 +0100

    Use 'pointerup' event for toggle event listener

--- color-scheme.js
+++ color-scheme.js
@@ -4,8 +4,9 @@ const color_preference = localStorage.getItem('color-preference') || browser_col
 document.documentElement.dataset.prefersColorScheme = color_preference;
 localStorage.setItem('color-preference', color_preference);
 
-document.querySelector('.set-color-scheme').addEventListener('click', e => {
-    const new_color_preference = localStorage.getItem('color-preference') === 'dark' ? 'light' : 'dark';
-    document.documentElement.dataset.prefersColorScheme = new_color_preference;
-    localStorage.setItem('color-preference', new_color_preference);
-});
+document.querySelector('.set-color-scheme')
+    .addEventListener('pointerup', e => {
+        const new_color_preference = localStorage.getItem('color-preference') === 'dark' ? 'light' : 'dark';
+        document.documentElement.dataset.prefersColorScheme = new_color_preference;
+        localStorage.setItem('color-preference', new_color_preference);
+    });
